ee114d008b284bad5a87c39f173164b20f4f6b17
MINGJI <106254888+MiN6JI@users.noreply.github.com>
2023-02-18 09:13:36 +0530

    Update New_Quiz.vue

--- src/pages/New_Quiz.vue
+++ src/pages/New_Quiz.vue
@@ -4,68 +4,199 @@
       src="src/assets/New Quiz.jpg"
       :ratio="16/9" 
     />
-    <div class="absolute-center" id="main">
-        <div id="header" class="row item-start justify-between">
-            <div id="NewQuiz"  class="text-h6"><strong>New Quiz</strong></div>
-            <div id="info">MINGJI</div>
+    <div class="row content-start absolute-center" id="main">
+                            
+                    <!-- |   HEADER  | -->
+
+        <div id="header" class="q-my-lg">
+            <div id="NewQuiz"  class="float-left text-h6"><strong>New Quiz</strong></div>
+            <div id="info" class="float-right q-py-sm">MINGJI</div>
+        </div>
+        
+                    <!-- |   Q n A  | -->
+
+        <div v-if = "index < count">
+        <div id="qna" class="q-my-lg -pt-lg">
+
+            <p>Questions 0{{ index + 1 }}</p>
+            <p class="text-h4 text-weight-bold">{{ questions[index]['question'] }}</p>
         </div>
 
-        <div id="qnA" class="q-mt-lg -pt-lg">
-            <p>Questions 00/05</p>
-            <p class="text-h3">This is where the question comes</p>
+                    <!-- |   OPTIONS   | -->
+
+        <div id="content">
+
+        <div class="">
+            <label 
+            :for="key" 
+            class="q-mr-lg q-mt-lg q-pa-md" 
+            v-for="(opt,key) in questions[index]['options']"
+            :key="opt"
+            :class="{'bg-black' : selectedAnswer == key}"
+            >
+            <input type="radio" 
+            :id="key"
+            :value="key"
+            class="hidden" 
+            @change="selected($event)"
+            />{{ opt }}
+            </label>
         </div>
-        <label class="">
-            <q-input type="radio" color="teal"  outlined />option 1
-        </label>
-        <label>
-            <q-input type="radio" color="teal" outlined/>option 2
-        </label>
+
+                    <!-- |   BUTTONS   | -->
+
+        <div class="q-mt-lg">
+            <q-btn 
+            unelevated
+            color="black"
+            label="Next"
+            class="float-right q-mt-lg"
+            @click="next(key)"
+            v-if="selectedAnswer != '' && index < count - 1"
+          ></q-btn>
+            <q-btn
+            outline
+            class="q-mt-lg"
+            color="black"
+            label="Previous"
+            @click="prev()"
+            v-if="index >= 1"
+            >
+            </q-btn>
+            <q-btn 
+            unelevated
+            color="black"
+            label="submit"
+            class="float-right q-mt-lg"
+            @click="submit()"
+            v-if="selectedAnswer != '' && index == count - 1"
+          ></q-btn>
         </div>
+
+        </div>
+
+    </div>
+
+                    <!-- |   RESULT  | -->
+
+    <div v-else class="" id="result">
+        <div class="text-h3 text-weight-bold">Result</div>
+        <div class="q-mt-lg">
+            <p class="text-h6 text-weight-light">Total score out of 
+                <strong><span>{{ correctAnswer }}</span></strong>
+            </p>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
+
     export default {
         data() {
         return {
             index: 0,
             selectedAnswer: '',
-            answer: 0,
+            correctAnswer: 0,
             wrongAnswer: 0,
             count: 5,
             questions: [
                 {
                     question: "Chlorofluorocarbons are accountable for which of the following process?",
-                    options: {a: 'Ozone depletion', b: 'Acid rain', c: 'Soil pollution', d: 'Genetic mutation'},
-                    answer: 'a'
+                    options: {a: 'Ozone depletion', b: 'Acid rain', c: 'Soil pollution'},
+                    // answer: 'c'
+                    // answer:  {a: 3, b: 2, c: 1}
                 },
                 {
                     question: "Which of the following gases is responsible for bleaching of plants?",
-                    options: {a: 'NO2', b: 'SO3', c: 'SO2', d: 'CO2'},
-                    answer: 'c'
+                    options: {a: 'NO2', b: 'SO3', c: 'SO2'},
+                    // answer: 'c'
                 },
                 {
                     question: "Which of the following water disinfecting procedures results in maximum wastage of water?",
-                    options: {a: ' Boiling', b: 'Reverse Osmosis (RO)', c: 'Adding chlorine', d: 'Irradiation'},
-                    answer: 'c'
+                    options: {a: ' Boiling', b: 'Reverse Osmosis (RO)', c: 'Adding chlorine'},
+                    // answer: 'c'
                 },
                 {
                     question: "Why is it recommended to avoid getting wet in the first rain?",
-                    options: {a: 'It will damage the clothes', b: 'First rain may contain harmful pollutants', c: ' It will spoil the shoes', d: 'None of the above'},
-                    answer: 'b'
+                    options: {a: 'It will damage the clothes', b: 'First rain may contain harmful pollutants', c: ' It will spoil the shoes'},
+                    // answer: 'b'
                 },
                 {
-                    question: "Inside of car getting warmer than outside because of sunligh",
-                    options: {a: 'Heat', b: 'Change of weather', c: 'Lightning', d: 'None of the above'},
-                    answer: 'a'
+                    question: "Inside of car getting warmer than outside because of sunlight",
+                    options: {a: 'Heat', b: 'Change of weather', c: 'Lightning'},
+                    // answer: 'a'
                 }
             ]
         }
+    },
+    methods: {
+        selected(e){
+            this.selectedAnswer = e.target.value
+            console.log(this.selectedAnswer);
+ 
+            if (this.selectedAnswer === "a") {
+                this.correctAnswer = this.correctAnswer + 3;
+                console.log(this.correctAnswer + "")
+            } else if (this.selectedAnswer === "b") {
+                this.correctAnswer = this.correctAnswer + 2;
+                console.log(this.correctAnswer + "")
+            } else {
+                this.correctAnswer = this.correctAnswer + 1;
+                console.log(this.correctAnswer + "")
+            }
+
+            // for(questions[1]['options']){
+            //     if (this.selectedAnswer === "a") {
+            //     this.correctAnswer = this.correctAnswer + 3;
+            //     console.log(this.correctAnswer + "")
+            // } else if (this.selectedAnswer === "b") {
+            //     this.correctAnswer = this.correctAnswer + 2;
+            //     console.log(this.correctAnswer + "")
+            // } else {
+            //     this.correctAnswer = this.correctAnswer + 1;
+            //     console.log(this.correctAnswer + "")
+            // }
+            // }
+        },
+        next() {
+        this.index++
+        this.selectedAnswer = ''
+        },
+        prev() {
+         this.index--
+        },
+        submit(){
+        this.index++   
+        }
+
+
     }
 }
 </script>
 <style scoped>
 #main{
     width: 80%;
+    height: 100vh;
+}
+#header{
+    width: 100%;
+    height: 100px;
+}
+label{
+    border: 1px solid black ;
+    /* display: block; */
+}
+#qna{
+    height: 150px;
+}
+
+#content{
+    height: 300px;
+}
+#result{
+    width: 100%;
+    height: 200px;
 }
 
 </style>
